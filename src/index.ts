9ca0954ba3dfc0d197bd4133dcc430e2d80417ff
kkpagaev <kkpagaev@gmail.com>
2023-05-30 01:04:29 +0300

    main func

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,25 @@ import { Command } from "commander"
 export function hello(str: string) {
   return `Hello ${str}`
 }
-const program = new Command()
 
-program
-  .name("string-util")
-  .description("CLI to some JavaScript string utilities")
-  .version("0.8.0")
+export function main() {
+  const program = new Command()
 
-program
-  .command("split")
-  .description("Split a string into substrings and display as an array")
-  .argument("<string>", "string to split")
-  .option("--first", "display just the first substring")
-  .option("-s, --separator <char>", "separator character", ",")
-  .action((str, options) => {
-    const limit = options.first ? 1 : undefined
-    console.log(str.split(options.separator, limit))
-  })
+  program
+    .name("string-util")
+    .description("CLI to some JavaScript string utilities")
+    .version("0.8.0")
 
-program.parse()
+  program
+    .command("split")
+    .description("Split a string into substrings and display as an array")
+    .argument("<string>", "string to split")
+    .option("--first", "display just the first substring")
+    .option("-s, --separator <char>", "separator character", ",")
+    .action((str, options) => {
+      const limit = options.first ? 1 : undefined
+      console.log(str.split(options.separator, limit))
+    })
+
+  program.parse()
+}
