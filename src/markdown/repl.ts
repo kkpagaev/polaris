b09fc634acf4a5800724c0f6d3971ea1cca344d9
kkpagaev <kkpagaev@gmail.com>
2023-06-06 01:57:12 +0300

    init basic markdown lexer

---
+++ src/markdown/repl.ts
@@ -0,0 +1,22 @@
+import { Tokenizer } from "./lexer"
+import readline from "readline"
+
+const rs = readline.createInterface({
+  input: process.stdin,
+})
+
+rs.on("line", (input) => {
+  const tokenizer = new Tokenizer(input)
+
+  while (true) {
+    const token = tokenizer.getNextToken()
+    console.log(token)
+    if (token.type === "EOF") {
+      break
+    }
+  }
+})
+
+rs.on("close", () => {
+  console.log("suck it chat")
+})
